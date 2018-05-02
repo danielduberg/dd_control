b70a4ddb504d46e03cd622bc4d501adcdac3716a
Daniel Duberg <danielduberg@gmail.com>
2018-05-02 15:52:03 +0200

    Fixed

--- include/dd_control/control.h
+++ include/dd_control/control.h
@@ -2,6 +2,8 @@
 #include <geometry_msgs/TwistStamped.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <mavros_msgs/RCIn.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 
 namespace dd_control
 {
@@ -10,6 +12,10 @@ class Control
 private:
   ros::NodeHandle nh_;
 
+  // Transform
+  tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener* tf_listener_;
+
   ros::Subscriber collision_free_control_sub_;
   ros::Subscriber setpoint_sub_;
   ros::Subscriber rc_sub_;
@@ -17,6 +23,8 @@ private:
   ros::Publisher control_pub_;
 
   int mode_;
+
+  std::string robot_frame_;
 
 public:
   Control(ros::NodeHandle& nh);
--- src/control.cpp
+++ src/control.cpp
@@ -3,18 +3,22 @@
 
 namespace dd_control
 {
-Control::Control(ros::NodeHandle& nh) : nh_(nh), mode_(0)
+Control::Control(ros::NodeHandle& nh) : nh_(nh), mode_(1)
 {
+  tf_listener_ = new tf2_ros::TransformListener(tf_buffer_);
+
+  robot_frame_ = "base_link";
+
   collision_free_control_sub_ = nh_.subscribe<geometry_msgs::TwistStamped>(
-      "/collision_free_control", 1, &Control::collisionFreeControlCallback,
+      "/collision_free_control", 10, &Control::collisionFreeControlCallback,
       this);
   setpoint_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>(
-      "/setpoint", 1, &Control::setpointCallback, this);
-  rc_sub_ = nh_.subscribe<mavros_msgs::RCIn>("/mavros/rc/in", 1,
+      "/setpoint", 10, &Control::setpointCallback, this);
+  rc_sub_ = nh_.subscribe<mavros_msgs::RCIn>("/mavros/rc/in", 10,
                                              &Control::rcCallback, this);
 
   control_pub_ = nh_.advertise<geometry_msgs::TwistStamped>(
-      "/mavros/setpoint_velocity/cmd_vel", 1);
+      "/mavros/setpoint_velocity/cmd_vel", 10);
 }
 
 void Control::collisionFreeControlCallback(
@@ -26,21 +30,37 @@ void Control::collisionFreeControlCallback(
   }
 }
 
-void Control::setpointCallback(
-    const geometry_msgs::PoseStamped::ConstPtr& msg)
+void Control::setpointCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
   if (mode_ == 1)
   {
     geometry_msgs::TwistStamped out;
-    out.header = msg->header;
-    out.twist.linear.x = std::min(msg->pose.position.x, 0.25);
-    out.twist.linear.x = std::max(out.twist.linear.x, -0.25);
-    out.twist.linear.y = std::min(msg->pose.position.y, 0.25);
-    out.twist.linear.y = std::max(out.twist.linear.y, -0.25);
-    out.twist.linear.z = std::min(msg->pose.position.z, 0.25);
-    out.twist.linear.z = std::max(out.twist.linear.z, -0.25);
-    out.twist.angular.z = std::min(tf2::getYaw(msg->pose.orientation), 0.17);
-    out.twist.angular.z = std::max(out.twist.angular.z, -0.17);
+    try
+    {
+      geometry_msgs::TransformStamped transform = tf_buffer_.lookupTransform(
+          robot_frame_, msg->header.frame_id, ros::Time(0));
+
+      geometry_msgs::PoseStamped setpoint_transformed;
+      tf2::doTransform(*msg, setpoint_transformed, transform);
+
+      out.header = setpoint_transformed.header;
+
+      out.twist.linear.x = std::min(setpoint_transformed.pose.position.x, 0.25);
+      out.twist.linear.x = std::max(out.twist.linear.x, -0.25);
+      out.twist.linear.y = std::min(setpoint_transformed.pose.position.y, 0.25);
+      out.twist.linear.y = std::max(out.twist.linear.y, -0.25);
+      out.twist.linear.z = std::min(setpoint_transformed.pose.position.z, 0.25);
+      out.twist.linear.z = std::max(out.twist.linear.z, -0.25);
+      out.twist.angular.z =
+          std::min(tf2::getYaw(setpoint_transformed.pose.orientation), 0.17);
+      out.twist.angular.z = std::max(out.twist.angular.z, -0.17);
+    }
+    catch (tf2::TransformException& ex)
+    {
+      ROS_WARN_THROTTLE(1, "DD Control: %s", ex.what());
+
+      out.header = msg->header;
+    }
 
     control_pub_.publish(out);
   }
