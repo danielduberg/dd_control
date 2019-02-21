a0269498dcec9f222bb64e3aa5dabf0f149468bf
Daniel Duberg <danielduberg@gmail.com>
2019-02-21 23:20:02 +0100

    Made the controller into a PID controller that can be configured through dynamic reconfigure

--- include/dd_control/control.h
+++ include/dd_control/control.h
@@ -1,12 +1,13 @@
-#include <ros/ros.h>
-#include <geometry_msgs/TwistStamped.h>
+#include <dd_control/ControlConfig.h>
+#include <dd_control/Velocity.h>
+#include <dynamic_reconfigure/server.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/TwistStamped.h>
 #include <mavros_msgs/RCIn.h>
+#include <nav_msgs/Odometry.h>
+#include <ros/ros.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
-#include <dynamic_reconfigure/server.h>
-#include <dd_control/ControlConfig.h>
 
 namespace dd_control
 {
@@ -21,16 +22,14 @@ private:
   tf2_ros::TransformListener* tf_listener_;
 
   // Subscribers
-  ros::Subscriber velocity_sub_;
   ros::Subscriber setpoint_sub_;
   ros::Subscriber cancel_sub_;
-  ros::Subscriber rc_sub_;
   ros::Subscriber pose_sub_;
-  ros::Subscriber current_velocity_sub_;
-  ros::Subscriber odom_sub_;
+  ros::Subscriber velocity_sub_;
 
   // Publishers
   ros::Publisher control_pub_;
+  ros::Publisher velocity_pub_;
 
   // Timers
   ros::Timer pub_timer_;
@@ -39,14 +38,9 @@ private:
   dynamic_reconfigure::Server<dd_control::ControlConfig> cs_;
   dynamic_reconfigure::Server<dd_control::ControlConfig>::CallbackType f_;
 
-  int mode_;
-
-  std::string output_frame_;
-
   geometry_msgs::PoseStamped last_setpoint_;
   geometry_msgs::PoseStamped current_pose_;
-  geometry_msgs::TwistStamped current_global_velocity_;
-  geometry_msgs::TwistStamped current_local_velocity_;
+  geometry_msgs::TwistStamped current_velocity_;
 
   // Max velocity
   double max_xy_vel_;
@@ -56,32 +50,34 @@ private:
   double k_p_xy_;
   double k_i_xy_;
   double k_d_xy_;
+  double bias_xy_;
+
+  double error_prior_xy_;
+  double integral_xy_;
 
   // Z PID-controller
   double k_p_z_;
   double k_i_z_;
   double k_d_z_;
+  double bias_z_;
+
+  double error_prior_z_;
+  double integral_z_;
 
   // Frequency
-  int frequency_;
+  double frequency_;
 
 public:
   Control(ros::NodeHandle& nh, ros::NodeHandle& nh_priv);
 
 private:
-  void velocityCallback(const geometry_msgs::TwistStamped::ConstPtr& msg);
-
   void setpointCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
 
   void cancelCallback(const std_msgs::Header::ConstPtr& msg);
 
-  void rcCallback(const mavros_msgs::RCIn::ConstPtr& msg);
-
   void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
 
-  void currentVelocityCallback(const geometry_msgs::TwistStamped::ConstPtr& msg);
-
-  void odomCallback(const nav_msgs::Odometry::ConstPtr& msg);
+  void velocityCallback(const geometry_msgs::TwistStamped::ConstPtr& msg);
 
   void timerPublish(const ros::TimerEvent& event);
 
--- src/control.cpp
+++ src/control.cpp
@@ -4,109 +4,40 @@
 namespace dd_control
 {
 Control::Control(ros::NodeHandle& nh, ros::NodeHandle& nh_priv)
-  : nh_(nh), nh_priv_(nh_priv), cs_(nh_priv_), mode_(1)
+  : nh_(nh), nh_priv_(nh_priv), cs_(nh_priv_), error_prior_xy_(0), integral_xy_(0), error_prior_z_(0), integral_z_(0)
 {
   // Set up dynamic reconfigure server
   f_ = boost::bind(&Control::configCallback, this, _1, _2);
   cs_.setCallback(f_);
 
-  output_frame_ = nh_priv_.param<std::string>("output_frame", "map");
-  std::string velocity_topic = nh_priv_.param<std::string>("velocity_topic", "/collision_"
-                                                                             "free_"
-                                                                             "control");
   std::string setpoint_topic = nh_priv_.param<std::string>("setpoint_topic", "/setpoint");
-  std::string rc_topic = nh_priv_.param<std::string>("rc_topic", "/mavros/rc/in");
   std::string pose_topic = nh_priv_.param<std::string>("pose_topic", "/mavros/"
                                                                      "local_position/"
                                                                      "pose");
-  std::string current_velocity_topic =
-      nh_priv_.param<std::string>("current_velocity_topic", "/mavros/local_position/"
-                                                            "velocity");
-  std::string odom_topic = nh_priv_.param<std::string>("odom_topic", "/mavros/"
-                                                                     "local_position/"
-                                                                     "odom");
+  std::string velocity_topic = nh_priv_.param<std::string>("velocity_topic", "/mavros/local_position/"
+                                                                             "velocity_local");
   std::string out_topic = nh_priv_.param<std::string>("out_topic", "/mavros/"
                                                                    "setpoint_position/"
                                                                    "local");
 
   tf_listener_ = new tf2_ros::TransformListener(tf_buffer_);
 
-  velocity_sub_ = nh_.subscribe<geometry_msgs::TwistStamped>(
-      velocity_topic, 10, &Control::velocityCallback, this);
-  setpoint_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>(
-      setpoint_topic, 10, &Control::setpointCallback, this);
-  cancel_sub_ = nh_.subscribe<std_msgs::Header>(setpoint_topic + "/cancel", 10,
-                                                &Control::cancelCallback, this);
-  rc_sub_ = nh_.subscribe<mavros_msgs::RCIn>(rc_topic, 10, &Control::rcCallback, this);
-  pose_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>(pose_topic, 10,
-                                                        &Control::poseCallback, this);
-  current_velocity_sub_ = nh_.subscribe<geometry_msgs::TwistStamped>(
-      current_velocity_topic, 10, &Control::currentVelocityCallback, this);
-  odom_sub_ =
-      nh_.subscribe<nav_msgs::Odometry>(odom_topic, 10, &Control::odomCallback, this);
+  setpoint_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>(setpoint_topic, 10, &Control::setpointCallback, this);
+  cancel_sub_ = nh_.subscribe<std_msgs::Header>(setpoint_topic + "/cancel", 10, &Control::cancelCallback, this);
+  pose_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>(pose_topic, 10, &Control::poseCallback, this);
+  velocity_sub_ = nh_.subscribe<geometry_msgs::TwistStamped>(velocity_topic, 10, &Control::velocityCallback, this);
 
   control_pub_ = nh_.advertise<geometry_msgs::PoseStamped>(out_topic, 10);
-}
-
-void Control::velocityCallback(const geometry_msgs::TwistStamped::ConstPtr& msg)
-{
-  pub_timer_.stop();
-  if (0 == mode_)
-  {
-    geometry_msgs::PoseStamped setpoint;
-    setpoint.header = msg->header;
-
-    // http://robotsforroboticists.com/pid-control/
-    double desired_x_vel = clamp(msg->twist.linear.x, -max_xy_vel_, max_xy_vel_);
-    double desired_y_vel = clamp(msg->twist.linear.y, -max_xy_vel_, max_xy_vel_);
-    double desired_z_vel = clamp(msg->twist.linear.z, -max_z_vel_, max_z_vel_);
-
-    double error_x_vel = desired_x_vel - current_local_velocity_.twist.linear.x;
-    double error_y_vel = desired_y_vel - current_local_velocity_.twist.linear.y;
-    double error_z_vel = desired_z_vel - current_local_velocity_.twist.linear.z;
-
-    setpoint.pose.position.x = k_p_xy_ * error_x_vel;
-    setpoint.pose.position.y = k_p_xy_ * error_y_vel;
-    setpoint.pose.position.z = k_p_z_ * error_z_vel;
-
-    tf2::Quaternion q;
-    q.setRPY(msg->twist.angular.y, msg->twist.angular.x, msg->twist.angular.z);
-
-    setpoint.pose.orientation.x = q.x();
-    setpoint.pose.orientation.y = q.y();
-    setpoint.pose.orientation.z = q.z();
-    setpoint.pose.orientation.w = q.w();
-
-    geometry_msgs::PoseStamped out;
-    try
-    {
-      geometry_msgs::TransformStamped transform = tf_buffer_.lookupTransform(
-          current_pose_.header.frame_id, setpoint.header.frame_id, ros::Time(0));
-
-      tf2::doTransform(setpoint, out, transform);
-    }
-    catch (tf2::TransformException& ex)
-    {
-      ROS_WARN_THROTTLE(1, "DD Control: %s", ex.what());
-
-      out.header = setpoint.header;
-      out.pose = current_pose_.pose;
-    }
-
-    control_pub_.publish(out);
-  }
-  pub_timer_.start();
+  velocity_pub_ = nh_priv_.advertise<dd_control::Velocity>("velocity", 10);
 }
 
 void Control::setpointCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
   pub_timer_.stop();
-  if (1 == mode_)
-  {
-    last_setpoint_ = *msg;
 
-    setpointPublish(last_setpoint_);
-  }
+  last_setpoint_ = *msg;
+  setpointPublish(last_setpoint_);
+
   pub_timer_.start();
 }
 
@@ -115,40 +46,14 @@ void Control::cancelCallback(const std_msgs::Header::ConstPtr& msg)
   last_setpoint_ = current_pose_;
 }
 
-void Control::rcCallback(const mavros_msgs::RCIn::ConstPtr& msg)
-{
-  if (msg->channels[6] < 1100)
-  {
-    mode_ = 0;
-  }
-  else if (msg->channels[6] >= 1100 && msg->channels[6] <= 1900)
-  {
-    mode_ = 1;
-  }
-  else
-  {
-    mode_ = 2;
-  }
-}
-
 void Control::poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
 {
   current_pose_ = *msg;
 }
 
-void Control::currentVelocityCallback(const geometry_msgs::TwistStamped::ConstPtr& msg)
+void Control::velocityCallback(const geometry_msgs::TwistStamped::ConstPtr& msg)
 {
-  current_global_velocity_ = *msg;
-}
-
-void Control::odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
-{
-  current_pose_.header = msg->header;
-  current_pose_.pose = msg->pose.pose;
-
-  current_local_velocity_.header = msg->header;
-  current_local_velocity_.header.frame_id = msg->child_frame_id;
-  current_local_velocity_.twist = msg->twist.twist;
+  current_velocity_ = *msg;
 }
 
 void Control::timerPublish(const ros::TimerEvent& event)
@@ -157,44 +62,77 @@ void Control::timerPublish(const ros::TimerEvent& event)
   setpointPublish(last_setpoint_);
 }
 
+// error_prior = 0
+// integral = 0
+// KP = Some value you need to come up (see tuning section below)
+// KI = Some value you need to come up (see tuning section below)
+// KD = Some value you need to come up (see tuning section below)
+
+// while(1) {
+//     error = desired_value – actual_value
+//     integral = integral + (error*iteration_time)
+//     derivative = (error – error_prior)/iteration_time
+//     output = KP*error + KI*integral + KD*derivative + bias
+//     error_prior = error
+//     sleep(iteration_time)
+// }
+
 void Control::setpointPublish(const geometry_msgs::PoseStamped& setpoint)
 {
   geometry_msgs::PoseStamped out;
   try
   {
-    geometry_msgs::TransformStamped transform = tf_buffer_.lookupTransform(
-        current_pose_.header.frame_id, setpoint.header.frame_id, ros::Time(0));
+    geometry_msgs::TransformStamped transform =
+        tf_buffer_.lookupTransform(current_pose_.header.frame_id, setpoint.header.frame_id, ros::Time(0));
 
     tf2::doTransform(setpoint, out, transform);
   }
   catch (tf2::TransformException& ex)
   {
     ROS_WARN_THROTTLE(1, "DD Control: %s", ex.what());
-
-    out.header = setpoint.header;
+    out.header.frame_id = setpoint.header.frame_id;
     out.pose = current_pose_.pose;
   }
+  out.header.stamp = setpoint.header.stamp;
 
   // http:  // robotsforroboticists.com/pid-control/
-  double desired_x_vel = out.pose.position.x - current_pose_.pose.position.x;
-  double desired_y_vel = out.pose.position.y - current_pose_.pose.position.y;
-  double desired_z_vel =
-      clamp(out.pose.position.z - current_pose_.pose.position.z, -max_z_vel_, max_z_vel_);
 
-  double desired_xy_vel = std::hypot(desired_x_vel, desired_y_vel);
+  // XY
+  double desired_x = out.pose.position.x - current_pose_.pose.position.x;
+  double desired_y = out.pose.position.y - current_pose_.pose.position.y;
+  double desired_xy = std::min(std::hypot(desired_x, desired_y), max_xy_vel_);
+  double desired_xy_dir = std::atan2(desired_x, desired_y);
 
-  desired_x_vel *= std::min(1.0, max_xy_vel_ / desired_xy_vel);
-  desired_y_vel *= std::min(1.0, max_xy_vel_ / desired_xy_vel);
+  double current_xy = std::hypot(current_velocity_.twist.linear.x, current_velocity_.twist.linear.y);
 
-  double error_x_vel = desired_x_vel - current_global_velocity_.twist.linear.x;
-  double error_y_vel = desired_y_vel - current_global_velocity_.twist.linear.y;
-  double error_z_vel = desired_z_vel - current_global_velocity_.twist.linear.z;
+  double error_xy = desired_xy - current_xy;
+  integral_xy_ += (error_xy * (1.0 / frequency_));
+  double derivate_xy_ = (error_xy - error_prior_xy_) / (1.0 / frequency_);
+  error_prior_xy_ = error_xy;
+  double output_xy = (k_p_xy_ * error_xy) + (k_i_xy_ * integral_xy_) + (k_d_xy_ * derivate_xy_) + bias_xy_;
 
-  out.pose.position.x = current_pose_.pose.position.x + (k_p_xy_ * error_x_vel);
-  out.pose.position.y = current_pose_.pose.position.y + (k_p_xy_ * error_y_vel);
-  out.pose.position.z = current_pose_.pose.position.z + (k_p_z_ * error_z_vel);
+  out.pose.position.x = current_pose_.pose.position.x + (output_xy * std::sin(desired_xy_dir));
+  out.pose.position.y = current_pose_.pose.position.y + (output_xy * std::cos(desired_xy_dir));
+
+  // Z
+  double desired_z = clamp(out.pose.position.z - current_pose_.pose.position.z, -max_z_vel_, max_z_vel_);
+  double error_z = desired_z - current_velocity_.twist.linear.z;
+  integral_z_ += (error_z * (1.0 / frequency_));
+  double derivate_z_ = (error_z - error_prior_z_) / (1.0 / frequency_);
+  error_prior_z_ = error_z;
+  out.pose.position.z =
+      current_pose_.pose.position.z + ((k_p_z_ * error_z) + (k_i_z_ * integral_z_) + (k_d_z_ * derivate_z_) + bias_z_);
 
   control_pub_.publish(out);
+
+  dd_control::Velocity vel_msg;
+  vel_msg.header.stamp = out.header.stamp;
+  vel_msg.header.frame_id = out.header.frame_id;
+  vel_msg.current_xy_vel = current_xy;
+  vel_msg.current_z_vel = current_velocity_.twist.linear.z;
+  vel_msg.desired_xy_vel = desired_xy;
+  vel_msg.desired_z_vel = desired_z;
+  velocity_pub_.publish(vel_msg);
 }
 
 double Control::clamp(double value, double min, double max)
@@ -210,29 +148,22 @@ void Control::configCallback(dd_control::ControlConfig& config, uint32_t level)
   k_p_xy_ = config.k_p_xy;
   k_i_xy_ = config.k_i_xy;
   k_d_xy_ = config.k_d_xy;
+  bias_xy_ = config.bias_xy;
 
   k_p_z_ = config.k_p_z;
   k_i_z_ = config.k_i_z;
   k_d_z_ = config.k_d_z;
-
-  if (config.frequency != frequency_)
-  {
-    if (0 == frequency_)
-    {
-      pub_timer_.stop();
-    }
-    else
-    {
-      pub_timer_ = nh_.createTimer(ros::Rate(frequency_), &Control::timerPublish, this,
-                                   false, false);
-    }
-  }
-  else if (!pub_timer_.hasStarted() && 0 != frequency_)
-  {
-    pub_timer_ = nh_.createTimer(ros::Rate(frequency_), &Control::timerPublish, this,
-                                 false, false);
-  }
+  bias_z_ = config.bias_z;
 
   frequency_ = config.frequency;
+
+  if (0 == frequency_)
+  {
+    pub_timer_.stop();
+  }
+  else
+  {
+    pub_timer_ = nh_.createTimer(ros::Rate(frequency_), &Control::timerPublish, this, false, true);
+  }
 }
 }  // namespace dd_control
